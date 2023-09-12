9f64add888ffd497f2f7575423e6c79e475b5fc1
vekt0R-HUB <zealot.algo@gmail.com>
2023-09-12 14:59:43 -0400

    Initial commit

--- ClashRoyaleClanWarsAPI/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ClashRoyaleClanWarsAPI.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
