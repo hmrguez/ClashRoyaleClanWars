8eae26efc49cc81f7b92b3efd8dfcb5e593ed61c
vekt0R-HUB <zealot.algo@gmail.com>
2023-09-14 21:01:25 -0400

    Clean project

--- ClashRoyaleClanWarsApp/src/app/app.module.ts
+++ ClashRoyaleClanWarsApp/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './components/grid/grid.component';
+import { TableModule } from "primeng/table";
+import { BadgeModule } from "primeng/badge";
+import { ButtonModule } from "primeng/button";
+import { RippleModule } from "primeng/ripple";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    BadgeModule,
+    ButtonModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
