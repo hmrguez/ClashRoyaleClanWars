0bbcc543450306348465e25ed4263c635bbf4857
vekt0R-HUB <zealot.algo@gmail.com>
2023-12-02 18:09:17 -0500

    Fixed issue with resetting password

--- ClashRoyaleClanWarsAPI/ClashRoyaleClanWarsAPI.API/Controllers/UserController.cs
+++ ClashRoyaleClanWarsAPI/ClashRoyaleClanWarsAPI.API/Controllers/UserController.cs
@@ -71,13 +71,17 @@ public class UserController : ApiController
     }
 
     // PUT api/users/{id:guid}/password
-    [HttpPut("{id:guid}/password")]
-    public async Task<IActionResult> ChangePassword(Guid id, [FromBody] string password)
+    [HttpPut("password")]
+    public async Task<IActionResult> ChangePassword([FromBody] PasswordDto passwordDto)
     {
-        var command = new ChangePasswordCommand(id, password);
+        Console.WriteLine("Password: " + passwordDto.Id + " ," + passwordDto.Password);
+        
+        var command = new ChangePasswordCommand(passwordDto.Id, passwordDto.Password);
 
         var result = await _sender.Send(command);
 
         return result.IsSuccess ? NoContent() : Problem(result.Errors);
     }
 }
+
+public record PasswordDto(string Password, Guid Id);
