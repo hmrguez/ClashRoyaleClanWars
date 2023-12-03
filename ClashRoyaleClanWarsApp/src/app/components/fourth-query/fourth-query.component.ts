219e468d55cf8270a106605e84c159f0334ef30e
karendcl <89109943+karendcl@users.noreply.github.com>
2023-12-03 16:02:28 -0500

    fixed username change bug

--- ClashRoyaleClanWarsApp/src/app/components/fourth-query/fourth-query.component.ts
+++ ClashRoyaleClanWarsApp/src/app/components/fourth-query/fourth-query.component.ts
@@ -19,13 +19,23 @@ export class FourthQueryComponent {
   clans: IClanDto[] = []
   @ViewChild("grid") grid: GridComponent = {} as GridComponent;
 
-  quality={1:''}
+  //create dictionary for types of card
+  cardTypes = new Map<number, string>();
+
+  
+
+
 
   queryColumns: IColumn[] = [
     
     {
       header: 'Card Name',
       field: 'cardName',
+      type: ColumnType.String,
+    },
+    {
+      header: 'Card Type',
+      field: 'cardType',
       type: ColumnType.String,
     },
     {
@@ -45,13 +55,19 @@ export class FourthQueryComponent {
 
   constructor(public queryService: QueryService)
   {
+    this.cardTypes.set(0,'Unknown')
+    this.cardTypes.set(1,'Spell')
+    this.cardTypes.set(2,'Structure')
+    this.cardTypes.set(3,'Troop')
+
     this.queryService.getAll().subscribe((data)=>{
       data.forEach(element => {
         this.data.push({
           cardName: element.cardName,
           count: element.count,
           clanName: element.clanName,
-          queryId : this.count
+          queryId : this.count,
+          cardType : this.cardTypes.get(element.cardType)
         })
         this.count++
       });
